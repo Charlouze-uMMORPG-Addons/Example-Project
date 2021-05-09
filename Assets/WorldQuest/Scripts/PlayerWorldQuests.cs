a16c959949a5807584e458c161c529b8e05a4393
Charlouze <me@charlouze.com>
2021-05-10 00:17:46 +0200

    Mécaniques de base des WorldQuest

---
+++ Assets/WorldQuest/Scripts/PlayerWorldQuests.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace WorldQuest
+{
+    public struct WorldQuestDescriptor
+    {
+        public string name;
+        public string description;
+    }
+
+    public class PlayerWorldQuests : MonoBehaviour
+    {
+        public List<Manager> managers = new List<Manager>();
+
+        public bool participating => managers.Count > 0;
+
+        public void Register(Manager manager)
+        {
+            managers.Add(manager);
+        }
+
+        public void Unregister(Manager manager)
+        {
+            managers.Remove(manager);
+        }
+    }
+}
